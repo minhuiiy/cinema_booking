295d77698b1c7b1f8b123aae9cd03ccb81ce8805
Chung Nguyen Thanh <chunhthanhde.dev@gmail.com>
2025-02-07 20:54:34 +0700

    feat: home screen #20 - add Banner model and update diagram - implement BannerModel and BannerEntity with JSON serialization

---
+++ lib/data/models/movies/banner.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'banner.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BannerModel _$BannerModelFromJson(Map<String, dynamic> json) => BannerModel(
+      id: (json['id'] as num?)?.toInt(),
+      url: json['image'] as String?,
+    );
+
+Map<String, dynamic> _$BannerModelToJson(BannerModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'image': instance.url,
+    };
