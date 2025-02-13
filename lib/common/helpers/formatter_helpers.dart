2b133654cab51adb3b6e5e2a86975dced94ed07d
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2025-02-13 11:22:34 +0700

    feat: add BasicBackButton widget and improve formatter helpers for consistency

--- lib/common/helpers/formatter_helpers.dart
+++ lib/common/helpers/formatter_helpers.dart
@@ -1,9 +1,15 @@
+/*
+ * @ Author: Chung Nguyen Thanh <chunhthanhde.dev@gmail.com>
+ * @ Created: 2025-12-20 14:32:06
+ * @ Message: 🎯 Happy coding and Have a nice day! 🌤️
+ */
+
 import 'package:intl/intl.dart';
 
 extension HHmm on Duration {
   String formatHHmm() {
     //1:34:00.000000
-    final str = this.toString();
+    final str = toString();
 
     final texts = str.split(":");
     final textHour = texts[0].padLeft(2, '0');
@@ -15,15 +21,15 @@ extension HHmm on Duration {
 
 extension FormatNumber on int {
   String formatDecimalThousand() {
-    //1403 -> 1,403
-    var f = new NumberFormat.decimalPattern("en_US");
+    //2025 -> 2,025
+    var f = NumberFormat.decimalPattern("en_US");
     return f.format(this);
   }
 }
 
 extension FormatDate on int {
+  // ignore: non_constant_identifier_names
   String MMM_dd_yyyy() {
-    return DateFormat("MMM dd, yyyy")
-        .format(DateTime.fromMillisecondsSinceEpoch(this * 1000));
+    return DateFormat("MMM dd, yyyy").format(DateTime.fromMillisecondsSinceEpoch(this * 1000));
   }
 }
