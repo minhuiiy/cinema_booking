030231a8a97b59c0ab4134850660bed7fc00f163
Chung Nguyen Thanh <chunhthanhde.dev@gmail.com>
2025-02-25 22:00:45 +0700

    refactor: improve logging format in SimpleBlocObserver and comment out unused transition logging

--- lib/simple_bloc_observer.dart
+++ lib/simple_bloc_observer.dart
@@ -23,8 +23,8 @@ class SimpleBlocObserver extends BlocObserver {
 
     if (kDebugMode) {
       print(
-        '\n'
-        '$blue--------------------------------\n'
+        '$blue\n'
+        '--------------------------------\n'
         'Bloc  : ${bloc.runtimeType}     \n'
         'Event : ${event.toString()}     \n'
         '--------------------------------$reset\n',
@@ -39,8 +39,8 @@ class SimpleBlocObserver extends BlocObserver {
 
     if (kDebugMode) {
       print(
-        '\n'
-        '$red$bold════════════════════════════════════════\n'
+        '$red$bold\n'
+        '════════════════════════════════════════\n'
         '               BLOC ERROR               \n'
         '════════════════════════════════════════\n'
         'Bloc   : ${bloc.runtimeType}          \n\n'
@@ -51,24 +51,24 @@ class SimpleBlocObserver extends BlocObserver {
     }
 
     /// Called whenever a state transition occurs in any Bloc.
-    @override
-    void onTransition(Bloc bloc, Transition transition) {
-      super.onTransition(bloc, transition);
+    // @override
+    // void onTransition(Bloc bloc, Transition transition) {
+    //   super.onTransition(bloc, transition);
 
-      // LogHelper.info(
-      //   tag: "onTransition",
-      //   message:
-      //       '\n'
-      //       '════════════════════════════════════════\n'
-      //       '         BLOC STATE TRANSITION          \n'
-      //       '════════════════════════════════════════\n'
-      //       'Bloc: ${bloc.runtimeType}               \n'
-      //       '----------------------------------------\n'
-      //       '▶ FROM: ${transition.currentState}      \n'
-      //       '▶ TO:   ${transition.nextState}         \n'
-      //       '════════════════════════════════════════\n'
-      //       '\n',
-      // );
-    }
+    //   LogHelper.info(
+    //     tag: "onTransition",
+    //     message:
+    //         '\n'
+    //         '════════════════════════════════════════\n'
+    //         '         BLOC STATE TRANSITION          \n'
+    //         '════════════════════════════════════════\n'
+    //         'Bloc: ${bloc.runtimeType}               \n'
+    //         '----------------------------------------\n'
+    //         '▶ FROM: ${transition.currentState}      \n'
+    //         '▶ TO:   ${transition.nextState}         \n'
+    //         '════════════════════════════════════════\n'
+    //         '\n',
+    //   );
+    // }
   }
 }
